249c376657374d0f78144a89ecafcb0a728bd5c4
Ramprits <rampritsahani@outlook.in>
2019-07-30 14:23:02 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BaseService } from './shared/base.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, NavbarComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [BaseService]
 })
 export class AppModule { }
