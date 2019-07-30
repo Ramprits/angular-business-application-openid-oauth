cdf55dab975f9abc5fd66c0622f11ee23334c45f
Ramprits <rampritsahani@outlook.in>
2019-07-30 18:59:40 +0530

    1 file modified

--- src/app/components/tours/tours.module.ts
+++ src/app/components/tours/tours.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { TourAddComponent } from './tour-add/tour-add.component';
 import { TourDetailComponent } from './tour-detail/tour-detail.component';
 import { TourUpdateComponent } from './tour-update/tour-update.component';
